fd774c669606ea366962cbae889c939c89240e7a
Paul Michali <pc@michali.net>
2016-03-04 15:11:07 -0500

    Setting up runner

---
+++ whodunit-runner.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+"""Convenience wrapper for running whodunit directly from source tree."""
+
+
+from whodunit.whodunit import main
+
+
+if __name__ == '__main__':
+    main()
